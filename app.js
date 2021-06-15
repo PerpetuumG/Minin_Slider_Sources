28640708b3b98d57333e5a0d689cd7a4c20d2218
PerpetuumG <perpetuumg@mail.ru>
2021-06-15 17:59:23 +0300

    Добавление управления кнопками

--- app.js
+++ app.js
@@ -16,18 +16,28 @@ downBtn.addEventListener('click', () => {
     changeSlide('down');
 });
 
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
+});
+
 function changeSlide(direction) {
     if (direction === 'up') {
-        activeSlideIndex++;
+        activeSlideIndex++
         if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0;
-        } else if (direction === 'down') {
-            activeSlideIndex--;
-            if (activeSlideIndex < 0) {
-                activeSlideIndex = slidesCount - 1;
-            }
+            activeSlideIndex = 0
         }
     }
+    else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+
 
     const height = container.clientHeight;
     mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
